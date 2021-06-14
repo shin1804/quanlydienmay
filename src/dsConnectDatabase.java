3c5744ff00a5e924a81f6bdf52122adec754168f
= <=>
2021-06-14 19:32:48 +0700

    first commit

---
+++ src/dsConnectDatabase.java
@@ -0,0 +1,57 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class dsConnectDatabase {
+    String connectionString = "jdbc:sqlserver://localhost:1433;databaseName=dienmay;user=sa;password=Tuyen2101";   
+    Connection conn;
+    public dsConnectDatabase(){
+       
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(connectionString);
+            
+            if(conn != null){
+                System.out.println("Kết nối CSDL SQL Server thành công!");
+            }
+            else{
+                System.out.println("Kết nối CSDL SQL Server thất bại");
+            }
+        }
+        catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex.toString());
+        }   
+    }
+    
+    public ResultSet ExcuteQueryGetTable(String cauTruyVanSQL){
+        try {
+            Statement stmt = conn.createStatement();           
+            ResultSet rs = stmt.executeQuery(cauTruyVanSQL);
+            return rs;
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+            
+        return null;
+    }
+    public void ExcuteQueryUpdateDB(String cauTruyVanSQL){
+       
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(cauTruyVanSQL);
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+    
+}
